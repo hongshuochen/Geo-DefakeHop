56d183cfd176f98bf36b18bfbaf2aaa1cf13201e
Max <hongshuo@usc.edu>
2022-05-05 11:13:33 +0800

    push

---
+++ split/split_styleGAN.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    fps = list()
+    labels = list()
+    for root, dirs, files in os.walk("../data/anti-deepfake-data and code/data/authentic"):
+        for file in files:
+            if "_128" in root:
+                fp = os.path.join(root, file)
+                fps.append(fp)
+                labels.append(0)
+
+    for root, dirs, files in os.walk("../data/styleGAN-attn-gen/images"):
+        for file in files:
+            if "ema" in root:
+                fp = os.path.join(root, file)
+                fps.append(fp)
+                labels.append(1)
+
+    n = len(fps)
+    # 10% 10% 80%
+    X_train, X_test, y_train, y_test = train_test_split(fps, labels, test_size=0.8, random_state=777)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.5, random_state=888)
+    info = []
+    for i in range(len(X_train)):
+        info.append([X_train[i], y_train[i], "train"])
+    for i in range(len(X_val)):
+        info.append([X_val[i], y_val[i], "val"])
+    for i in range(len(X_test)):
+        info.append([X_test[i], y_test[i], "test"])
+    cols=['FP', 'isFake', 'SET']
+    df=pd.DataFrame(info,columns=cols)
+    df.to_csv("styleGAN-attn_10_10_80.csv", index=False)
